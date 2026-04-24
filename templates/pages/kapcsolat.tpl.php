465cbe03bfbe57f0b1690c1c9f1e7e09aa0168a6
maszity <aszitymilan@gmail.com>
2026-04-24 19:29:04 +0200

    Kapcsolat oldal frontend urlap

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -1,6 +1,29 @@
-<h2>Adatok:</h2>
-<p>Ügyvezető: <strong>Valaki Az</strong></p>
-<p>E-mail: <strong>valaki.az@minihonlap.hu</strong></p>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2726.3375296155727!2d19.66695091525771!3d46.89607994478184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c479e1d%3A0xc8292b3f6dc69e7f!2sPallasz+Ath%C3%A9n%C3%A9+Egyetem+GAMF+Kar!5e0!3m2!1shu!2shu!4v1475753185783" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-<br>
-<a target="_blank" href="https://www.google.hu/maps/place/Pallasz+Ath%C3%A9n%C3%A9+Egyetem+GAMF+Kar/@46.8960799,19.6669509,17z/data=!3m1!4b1!4m5!3m4!1s0x4743da7a6c479e1d:0xc8292b3f6dc69e7f!8m2!3d46.8960763!4d19.6691396?hl=hu">Nagyobb térkép</a>
+<form id="kapcsolat_form" method="POST" action="kapcsolat" enctype="multipart/form-data">
+
+    <fieldset>
+
+        <legend>
+            <h3>Vegye fel velünk a kapcsolatot!</h3>
+        </legend><br />
+
+        <div>
+            <label for="teljes_nev">Teljes név</label><br />
+            <input type="text" name="teljes_nev" id="teljes_nev" placeholder="Adja meg a teljes nevét" />
+        </div><br />
+
+        <div>
+            <label for="szoveg">Teljes név</label><br />
+            <textarea id="szoveg" name="szoveg" placeholder="Írja le üzenetét" rows="8" cols="64"></textarea>
+        </div><br />
+
+        <p style="color: red;" id="errorMessageCon">
+            <?php if(isset($errormessage)) { ?>
+                <?= $errormessage ?>
+            <?php } ?>
+        </p>
+
+        <input type="submit" id="kapcsolat_submit" name="submit" value="Küldés"><br>
+
+    </fieldset>
+
+</form>
